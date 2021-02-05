3a03b8c0791bb3a706e1c5eaf257ed170057aa62
f7deat <tan.dct9x@gmail.com>
2021-02-05 14:45:33 +0700

    add fontawesome branding

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,16 +1,18 @@
 import Header from "./components/Header"
 import { HappyNews, Mention, MobileLogin } from "./svg/UnDraw"
 import designer from './svg/designer.svg'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faExternalLinkAlt, faLongArrowAltRight } from "@fortawesome/free-solid-svg-icons"
 
 const Home = () => {
     return (
-        <div className="px-2 md:px-4 overflow-hidden relative text-gray-900" style={{ background: `url(${designer}) no-repeat`, backgroundPositionX: '100%' }}>
+        <div className="px-4 md:px-4 overflow-hidden relative text-gray-900" style={{ background: `url(${designer}) no-repeat`, backgroundPositionX: '100%' }}>
             <div className="items-center">
                 <Header />
                 <div className="flex items-center justify-center mb-4 h-screen">
                     <div>
                         <div className="text-center text-5xl font-bold">The Ultimate Company</div>
-                        <div className="text-5xl mb-3 font-bold text-center">
+                        <div className="text-5xl mb-6 font-bold text-center">
                             Business Management Solution
                             </div>
                         <div className="text-gray-500 text-xl mb-9">
@@ -19,12 +21,17 @@ const Home = () => {
                             </div>
                         </div>
                         <div className="text-center">
-                            <button className="py-3 px-4 text-xl bg-gray-800 text-white rounded mr-3 font-bold hover:bg-gray-900 shadow">Get Started Now</button>
-                            <button className="py-3 px-4 text-xl bg-white text-gray-800 rounded font-bold hover:bg-gray-900 hover:text-white shadow">Try Demo</button>
+                            <button className="py-3 px-4 text-xl bg-gray-800 text-white rounded mr-3 font-bold hover:bg-gray-900 shadow mb-3">
+                                Get Started Now <FontAwesomeIcon icon={faLongArrowAltRight} />
+                            </button>
+                            <button className="py-3 px-4 text-xl bg-white text-gray-800 rounded font-bold hover:bg-gray-900 hover:text-white shadow">
+                                Try Demo <FontAwesomeIcon icon={faExternalLinkAlt}/>
+                            </button>
                         </div>
                     </div>
                 </div>
             </div>
+            <div className="md:p-20">
             <div className="md:flex mt-10 items-center mb-20">
                 <div className="md:w-1/2 flex justify-center">
                     <MobileLogin />
@@ -51,6 +58,7 @@ const Home = () => {
                     <div className="text-3xl font-bold mb-4">Priority support</div>
                     <div className="text-gray-500">Connecting you to our highly-qualified support team 24/7/365</div>
                 </div>
+            </div>
             </div>
         </div>
     )
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,42 @@
-import { faBaseballBall } from "@fortawesome/free-solid-svg-icons"
+import { faFacebook, faInstagram, faLinkedin, faTwitter, faYoutube } from "@fortawesome/free-brands-svg-icons"
+import { faLayerGroup } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 const Footer = () => {
     return (
-        <div className="bg-gray-200 px-2 md:px-4 py-4">
-            Copyright 2020 <b>Crispy</b>
-            <div className="flex justify-end">
-                <FontAwesomeIcon icon={faBaseballBall} />
+        <div className="bg-gray-800 text-gray-100 p-4 md:px-20 md:py-10 mt-10">
+            <div className="md:flex items-center">
+                <div className="md:w-3/4">
+                    <div className="mb-3 flex items-center">
+                        <FontAwesomeIcon icon={faLayerGroup} size="3x" />
+                        <div className="text-2xl font-bold ml-4">Waffle</div>
+                    </div>
+                    <div className="py-5">
+                        <a href="/term" className="text-gray-400 hover:text-gray-300 mr-10">Term & Conditions</a>
+                        <a href="/privacy" className="text-gray-400 hover:text-gray-300 mr-10">Privacy Policy</a>
+                        <a href="/access" className="text-gray-400 hover:text-gray-300 mr-10">Accessbility</a>
+                        <a href="/legal" className="text-gray-400 hover:text-gray-300">Legal</a>
+                    </div>
+                </div>
+                <div className="md:w-1/4">
+                    <div className="mb-2 font-medium">Get the freshest news from us</div>
+                    <div className="flex">
+                        <input type="text" placeholder="Enter your email" className="mr-2 px-4 rounded text-gray-900" />
+                        <button type="button" className="bg-blue-900 text-white px-6 py-2 hover:bg-blue-600 rounded font-medium">Subcribe</button>
+                    </div>
+                </div>
+            </div>
+            <div className="flex justify-between mt-10 border-t border-gray-600 pt-5">
+                <div className="">Copyright 2021 DefZone.Net</div>
+                <div>
+                    <a href="https://fb.me/tan.dct" target="_blank" rel="noreferrer" className="hover:text-blue-200"><FontAwesomeIcon icon={faFacebook} size="lg" className="mr-4"/></a>
+                    <FontAwesomeIcon icon={faTwitter} size="lg" className="mr-4"/>
+                    <FontAwesomeIcon icon={faYoutube} size="lg" className="mr-4"/>
+                    <FontAwesomeIcon icon={faInstagram} size="lg" className="mr-4"/>
+                    <a href="https://www.linkedin.com/company/defzone" target="_blank" rel="noreferrer" className="hover:text-blue-200">
+                        <FontAwesomeIcon icon={faLinkedin} size="lg"/>
+                    </a>
+                </div>
             </div>
         </div>
     )
